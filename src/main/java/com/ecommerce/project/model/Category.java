f74edd93c08dc09ed34d6ee329aa33a2abed47fa
prince21212 <princepandey21212@gmail.com>
2025-10-08 17:34:11 +0530

    cart and Authentication controller updation

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -1,57 +1,27 @@
 package com.ecommerce.project.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import jakarta.validation.executable.ValidateOnExecution;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+import java.util.List;
+
 @Entity(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
 
     @NotBlank
-    @Size(min = 4, message = "Category name must be at least 4 characters long")
+    @Size(min = 5, message = "Category name must contain atleast 5 characters")
     private String categoryName;
-/*
-    public Category(Long categoryId, String categoryName) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
 
-    public Category() {
-
-    }
-
-    public String getCategoryName() {
-
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-
-        this.categoryName = categoryName;
-    }
-
-    public Long getCategoryId() {
-
-        return categoryId;
-    }
-
-    public void setCategoryId(Long categoryId) {
-
-        this.categoryId = categoryId;
-    }
-    */
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Product> products;
 }
