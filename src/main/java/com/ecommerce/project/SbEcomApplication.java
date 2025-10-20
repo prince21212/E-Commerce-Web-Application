a4dea0b53805aa74180b45112c9f41bd03d278c5
prince21212 <princepandey21212@gmail.com>
2025-10-21 00:22:50 +0530

    Updates after re-clone

---
+++ src/main/java/com/ecommerce/project/SbEcomApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//why we are using @SpringBootApplication
+public class SbEcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbEcomApplication.class, args);
+	}
+}
